cc634ee6b1d129f91ec693735675f8a4e266940b
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2025-03-11 21:36:47 -0300

    feat: backlog e recebidos FM

--- js/script.js
+++ js/script.js
@@ -39,10 +39,17 @@ function initializeFileUploadHandlers() {
     },
   ];
 
-  uploadConfigs.forEach(config => setupFileUploadHandlers(config));
+  uploadConfigs.forEach((config) => setupFileUploadHandlers(config));
 }
 
-function setupFileUploadHandlers({ dropArea, fileInput, loading, resultsTable, resultsBody, titulo }) {
+function setupFileUploadHandlers({
+  dropArea,
+  fileInput,
+  loading,
+  resultsTable,
+  resultsBody,
+  titulo,
+}) {
   const elements = {
     dropArea: document.getElementById(dropArea),
     fileInput: document.getElementById(fileInput),
@@ -57,13 +64,17 @@ function setupFileUploadHandlers({ dropArea, fileInput, loading, resultsTable, r
     event.preventDefault();
     elements.dropArea.classList.add("dragover");
   });
-  elements.dropArea.addEventListener("dragleave", () => elements.dropArea.classList.remove("dragover"));
+  elements.dropArea.addEventListener("dragleave", () =>
+    elements.dropArea.classList.remove("dragover")
+  );
   elements.dropArea.addEventListener("drop", (event) => {
     event.preventDefault();
     elements.dropArea.classList.remove("dragover");
     processFiles(event.dataTransfer.files, elements);
   });
-  elements.fileInput.addEventListener("change", () => processFiles(elements.fileInput.files, elements));
+  elements.fileInput.addEventListener("change", () =>
+    processFiles(elements.fileInput.files, elements)
+  );
 }
 
 function processFiles(files, elements) {
@@ -93,22 +104,65 @@ function processFiles(files, elements) {
   });
 }
 
+/**
+ * Inverte o formato de uma string de data de "DD-MM-YYYY" para "YYYY-MM-DD".
+ *
+ * @param {string} dateString - A string de data em formato "DD-MM-YYYY".
+ * @returns {string|null} - A string de data em formato "YYYY-MM-DD", ou null se o formato de entrada for inválido.
+ */
+function invertDateFormat(dateString) {
+  // Verifica se a data está no formato esperado
+  const parts = dateString.split("-");
+  if (parts.length !== 3) {
+    console.error("Formato de data inválido: ", dateString);
+    return null; // Retorna null se o formato não for válido
+  }
+
+  const [day, month, year] = parts;
+
+  // Retorna a data no formato "YYYY-MM-DD"
+  return `${year}-${month}-${day}`;
+}
+
+/**
+ * Converte uma string de data em formato "DD/MM/YYYY" para "YYYY-MM-DD"
+ *
+ * @param {string} dateString - A string de data em formato "DD/MM/YYYY"
+ * @returns {string|null} - A string de data em formato "YYYY-MM-DD", ou null
+ *                         se o formato de entrada for inválido.
+ */
+function convertDateFormat(dateString) {
+  // Verifica se a data está no formato esperado
+  const parts = dateString.split("/");
+  if (parts.length !== 3) {
+    console.error("Formato de data inválido: ", dateString);
+    return null; // Retorna null se o formato não for válido
+  }
+
+  const [day, month, year] = parts;
+
+  // Retorna a data no formato "YYYY-MM-DD"
+  return `${year}-${month}-${day}`;
+}
+
 // Modifique a função analyzeData para considerar o número de colunas corretas
 function analyzeData(data, elements) {
   let counts = {};
 
-  const formattedToday = getFormattedDate(new Date());
+  // const formattedToday = getFormattedDate(new Date());
+  const formattedToday = "2025-03-07";
   const isForwardOrder = elements.resultsTable.id === "results-table-forward";
   const isReturnOrder = elements.resultsTable.id === "results-table-return";
   const isPickupOrder = elements.resultsTable.id === "results-table-pickup";
-  const isBrAssignment = elements.resultsTable.id === "results-table-br-assignment";
+  const isBrAssignment =
+    elements.resultsTable.id === "results-table-br-assignment";
 
-  switch(elements.resultsTable.id) {
+  switch (elements.resultsTable.id) {
     case "results-table-forward":
-      counts = { totalCol1: 0, totalCol2: 0 };
+      counts = { totalCol1: 0, totalCol2: 0, totalCol3: 0 };
       break;
     case "results-table-return":
-      counts = { totalCol1: 0 };
+      counts = { totalCol1: 0, totalCol2: 0 };
       break;
     case "results-table-pickup":
       counts = { totalCol1: 0, totalCol2: 0, totalCol3: 0 };
@@ -124,25 +178,35 @@ function analyzeData(data, elements) {
     const deliveringTime = normalizeDate(row["Delivering Time"]);
     const deliveredTime = normalizeDate(row["Delivered Time"]);
     const onHoldTime = normalizeDate(row["OnHold Time"]);
+    const lmHubReceiveTime = normalizeDate(row["LM Hub Receive time"]);
+    const receivedTime = normalizeDate(row["Received Time"]);
     const status = row["Status"]?.trim();
+    const dcReceiveTime = normalizeDate(row["DC Receive Time"]);
 
-    switch(elements.resultsTable.id) {
+    switch (elements.resultsTable.id) {
       case "results-table-forward":
         if (deliveringTime === formattedToday) {
           counts.totalCol1++;
           if (deliveredTime === formattedToday) counts.totalCol2++;
         }
+        if (
+          status === "Hub_Received" &&
+          invertDateFormat(lmHubReceiveTime) < formattedToday
+        ) {
+          counts.totalCol3++;
+        }
         break;
       case "results-table-return":
         if (status === "Return_Hub_Received") {
           counts.totalCol1++;
+          if (invertDateFormat(receivedTime) < formattedToday) {
+            counts.totalCol2++;
+          }
         }
         break;
       case "results-table-pickup":
-        if (deliveringTime === formattedToday) {
+        if (convertDateFormat(dcReceiveTime) === formattedToday) {
           counts.totalCol1++;
-          if (deliveredTime === formattedToday) counts.totalCol2++;
-          if (onHoldTime === formattedToday) counts.totalCol3++;
         }
         break;
       case "results-table-br-assignment":
@@ -164,14 +228,16 @@ function analyzeData(data, elements) {
 // Modifique a função updateTable para aceitar apenas as colunas necessárias
 function updateTable(counts, elements) {
   let rowHTML = "<tr>";
-  
-  switch(elements.resultsTable.id) {
+
+  switch (elements.resultsTable.id) {
     case "results-table-forward":
       rowHTML += `<td>${counts.totalCol1}</td>`;
       rowHTML += `<td>${counts.totalCol2}</td>`;
+      rowHTML += `<td>${counts.totalCol3}</td>`;
       break;
     case "results-table-return":
       rowHTML += `<td>${counts.totalCol1}</td>`;
+      rowHTML += `<td>${counts.totalCol2}</td>`;
       break;
     case "results-table-pickup":
       rowHTML += `<td>${counts.totalCol1}</td>`;
@@ -184,14 +250,16 @@ function updateTable(counts, elements) {
       rowHTML += `<td>${counts.totalCol3}</td>`;
       break;
   }
-  
+
   rowHTML += "</tr>";
   elements.resultsBody.innerHTML = rowHTML;
   elements.resultsTable.style.display = "table";
 }
 
 function getFormattedDate(date) {
-  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getFullYear()}`;
+  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getFullYear()}`;
 }
 
 function normalizeDate(dateStr) {
@@ -199,8 +267,12 @@ function normalizeDate(dateStr) {
 }
 
 function initializeReportButtons() {
-  document.getElementById("generate-report").addEventListener("click", generateReport);
-  document.getElementById("capture-report").addEventListener("click", captureReport);
+  document
+    .getElementById("generate-report")
+    .addEventListener("click", generateReport);
+  document
+    .getElementById("capture-report")
+    .addEventListener("click", captureReport);
 }
 
 function generateReport() {
@@ -212,13 +284,37 @@ function generateReport() {
   document.getElementById("report-container").style.display = "none";
 
   setTimeout(() => {
-    const forwardValues = getTableValues("results-table-forward", ["Entregue", "OnHold"]);
-    const returnValues = getTableValues("results-table-return", ["Total Revamp Piso"]);
-    const pickupValues = getTableValues("results-table-pickup", ["Total Pickup", "Entregue", "Cancelado"]);
-    const brAssignmentValues = getTableValues("results-table-br-assignment", ["Total BRs", "Atribuído", "Não Atribuído"]);
+    const forwardValues = getTableValues("results-table-forward", [
+      "Entregue",
+      "OnHold",
+      "Hub Received",
+    ]);
+    const returnValues = getTableValues("results-table-return", [
+      "Total Revamp Piso",
+      "Return Hub Received",
+    ]);
+    const pickupValues = getTableValues("results-table-pickup", [
+      "Recebidos FM",
+      "Entregue",
+      "Cancelado",
+    ]);
+    const brAssignmentValues = getTableValues("results-table-br-assignment", [
+      "Total BRs",
+      "Atribuído",
+      "Não Atribuído",
+    ]);
+
+    const hubReceived = parseInt(forwardValues["Hub Received"]) || 0; // Converte para número
+    const returnHubReceived =
+      parseInt(returnValues["Return Hub Received"]) || 0; // Converte para número
+
+    console.log(hubReceived, returnHubReceived);
+
+    const backlogDiaAnterior = hubReceived + returnHubReceived; // Soma dos valores
 
     const reportData = [
-      // ["Total LM Expedido", forwardValues["Total LM Expedido"]],
+      ["Backlog do Dia Anterior", backlogDiaAnterior || "-"],
+      ["Recebidos FM", pickupValues["Recebidos FM"] || "-"],
       ["Entregue", forwardValues["Entregue"] || "-"],
       ["OnHold", forwardValues["OnHold"] || "-"],
       ["Total Revamp Piso", returnValues["Total Revamp Piso"] || "-"],
@@ -230,9 +326,9 @@ function generateReport() {
       ["Não Atribuído", brAssignmentValues["Não Atribuído"] || "-"],
     ];
 
-    reportData.forEach(rowData => {
+    reportData.forEach((rowData) => {
       const tr = document.createElement("tr");
-      rowData.forEach(col => {
+      rowData.forEach((col) => {
         const td = document.createElement("td");
         td.textContent = col;
         tr.appendChild(td);
@@ -241,7 +337,8 @@ function generateReport() {
     });
 
     const now = new Date();
-    const formattedDateTime = now.toLocaleDateString("pt-BR") + " " + now.toLocaleTimeString("pt-BR");
+    const formattedDateTime =
+      now.toLocaleDateString("pt-BR") + " " + now.toLocaleTimeString("pt-BR");
     document.getElementById("report-datetime").textContent = formattedDateTime;
 
     document.getElementById("loading").style.display = "none";
@@ -270,13 +367,17 @@ function getTableValues(tableId, keys) {
 function captureReport() {
   const reportDiv = document.getElementById("report-container");
 
-  html2canvas(reportDiv, { scale: 2 }).then(canvas => {
+  html2canvas(reportDiv, { scale: 2 }).then((canvas) => {
     const link = document.createElement("a");
     link.href = canvas.toDataURL("image/png");
 
     const now = new Date();
     const formattedDate = now.toLocaleDateString("pt-BR").replace(/\//g, "-");
-    const formattedTime = now.toLocaleTimeString("pt-BR").split(":").slice(0, 2).join("-");
+    const formattedTime = now
+      .toLocaleTimeString("pt-BR")
+      .split(":")
+      .slice(0, 2)
+      .join("-");
 
     link.download = `relatorio_${formattedDate}_${formattedTime}.png`;
     link.click();
